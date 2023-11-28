24e02e8877f2b00581bbea4df7bb0e153e7aef09
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-11-28 14:24:50 +0400

    v.1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,70 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String profileString = getProfileString(file);
+
+        if (profileString == null){
+            return null;
+        }
+
+        String[] lines = profileString.split("\n");
+
+        HashMap<String,String> profileMap = new HashMap<>();
+
+        for (String line:  lines) {
+            String[] words = line.split(":");
+
+            if (words.length != 2){
+                continue;
+            }
+            profileMap.put(words[0],words[1].trim());
+        }
+
+        Profile profile = new Profile();
+
+        profile.setName(profileMap.get("Name"));
+        profile.setAge(Integer.parseInt(profileMap.get("Age")));
+        profile.setEmail(profileMap.get("Email"));
+        profile.setPhone(Long.parseLong(profileMap.get("Phone")));
+
+        return profile;
+    }
+
+    private String getProfileString(File file){
+        String profileString = "";
+
+        try(RandomAccessFile aFile = new RandomAccessFile(file, "r");
+            FileChannel inChannel = aFile.getChannel();) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            StringBuilder content = new StringBuilder();
+
+            while (inChannel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); i++) {
+                    content.append((char) buffer.get());
+                }
+                buffer.clear(); // do something with the data and clear/compact it.
+            }
+
+            return content.toString();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
